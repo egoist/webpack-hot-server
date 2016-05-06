a2dce195e90430778595ccde618313757549fa83
EGOIST <0x142857@gmail.com>
2016-05-06 20:30:31 +0800

    fix

--- index.js
+++ index.js
@@ -6,56 +6,48 @@ const app = express()
 const self = this
 
 module.exports = function (options) {
-  return new Promise((resolve, reject) => {
-    const port = options.port || 3001
-    const config = options.config || {}
-    const compiler = webpack(config)
+  const config = options.config || {}
+  const compiler = webpack(config)
 
-    const devMiddleWare = require('webpack-dev-middleware')(compiler, Object.assign({}, {
-      publicPath: config.output.publicPath,
-      stats: {
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false
-      }
-    }, options.compiler || {}))
-    
-    const dist = Path.dirname(config.output.path)
-    devMiddleWare.fileSystem.mkdirpSync(config.output.path)
+  const devMiddleWare = require('webpack-dev-middleware')(compiler, Object.assign({}, {
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    }
+  }, options.compiler || {}))
 
-    app.use(self.middleware = devMiddleWare)
+  const dist = Path.dirname(config.output.path)
+  devMiddleWare.fileSystem.mkdirpSync(config.output.path)
 
-    app.use(require('webpack-hot-middleware')(compiler))
+  app.use(self.middleware = devMiddleWare)
 
-    app.get('*', (req, res) => {
-      const fs = self.middleware.fileSystem
-      self.middleware.waitUntilValid(() => {
-        if (options.customIndex) {
-          const fp = typeof options.customIndex === 'string'
-            ? options.customIndex
-            : config.output.path
-          const filename = options.filename || 'index.html'
-          const filepath = Path.join(fp, filename)
-          const contents = fs.readdirSync(dist)
-          const exists = contents.indexOf(filename) !== -1
-          if (exists) {
-            res.end(fs.readFileSync(filepath))
-          } else {
-            res.end('Refresh when bundle is valid...')
-          }
+  app.use(require('webpack-hot-middleware')(compiler))
+
+  app.get('*', (req, res) => {
+    const fs = self.middleware.fileSystem
+    self.middleware.waitUntilValid(() => {
+      if (options.customIndex) {
+        const fp = typeof options.customIndex === 'string'
+          ? options.customIndex
+          : config.output.path
+        const filename = options.filename || 'index.html'
+        const filepath = Path.join(fp, filename)
+        const contents = fs.readdirSync(dist)
+        const exists = contents.indexOf(filename) !== -1
+        if (exists) {
+          res.end(fs.readFileSync(filepath))
         } else {
-          res.sendFile(Path.join(__dirname, 'index.html'))
+          res.end('Refresh when bundle is valid...')
         }
-      })
-    })
-
-    app.listen(port, 'localhost', err => {
-      if (err) {
-        return reject(err)
+      } else {
+        res.sendFile(Path.join(__dirname, 'index.html'))
       }
-      resolve(port)
     })
   })
+
+  return app
 }
